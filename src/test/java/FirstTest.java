ba8346a970f1364ffe500e668d323488807fff95
Chen Harel <chook.harel@gmail.com>
2021-03-04 16:21:57 -0800

    circle

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+import junit.framework.TestCase;
+
+import junit.framework.*;
+// by FYICenter.com
+public class FirstTest extends TestCase {
+   protected double fValue1;
+   protected double fValue2;
+
+   protected void setUp() {
+       fValue1= 2.0;
+       fValue2= 3.0;
+   }
+
+   public void testAdd() {
+       double result= fValue1 + fValue2;
+       assertTrue(result == 5.0);
+   }
+}
